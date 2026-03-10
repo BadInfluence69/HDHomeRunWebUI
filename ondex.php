d698a957fe38f164158b49368907a381f4e976a7
BadInfluence69 <87334251+BadInfluence69@users.noreply.github.com>
2026-03-10 14:57:33 -0500

    Add ondex.php with iframe for media hub

---
+++ ondex.php
@@ -0,0 +1,10 @@
+<body style="margin: 0; padding: 0; overflow: hidden; background: #000;">
+    <iframe 
+        src="http://192.168.0.142:5001"
+        title="NORA Media Hub" 
+        style="border: none; width: 100vw; height: 100vh; display: block;"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="no-referrer" 
+        allowfullscreen>
+    </iframe>
+</body>
